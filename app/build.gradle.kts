c1c47bae1b3f6c97f16b324c15c15cbac3d0039e
usamajavaid <usama.javaid@o3interfaces.com>
2025-07-24 16:20:24 +0500

    Your commit message here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation(project(":shared"))
     implementation(project(":login"))
     implementation(project(":dashboard"))
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
